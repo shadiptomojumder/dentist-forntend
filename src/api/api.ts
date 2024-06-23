4868dd8723ca21bd50fa4762059315159f5a3ef3
shadipto mojumder <shadiptomojumder58k@gmail.com>
2024-06-23 18:33:28 +0600

    render

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: `https://dentist-backend-one.vercel.app/api`,
+  baseURL: `https://dentist-backend-phee.onrender.com/api`,
   timeout: 10000,
   headers: {'X-Custom-Header': 'foobar'},
   withCredentials: true
 });
 
 // https://dentist-backend-phee.onrender.com
-// https://dentist-backend-one.vercel.app/
+// https://dentist-backend-one.vercel.app
+// http://localhost:5000
