c447eec703c731139e2fb63cd0a07ade31c8bf3a
shadipto mojumder <shadiptomojumder58k@gmail.com>
2025-05-12 10:23:19 +0600

    Fix base URL in API configuration to include '/api' path

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
-const BaseURL = "https://dentist-backend-one.vercel.app";
+const BaseURL = "https://dentist-backend-one.vercel.app/api";
 
 export const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    baseURL: BaseURL,
     timeout: 10000,
     headers: { "X-Custom-Header": "foobar" },
     withCredentials: true,
